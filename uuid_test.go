720d242a66ee750b7808d23e920963231fd7ff38
Spriithy <theo.dano@gmail.com>
2016-11-05 16:38:49 +0100

    Added Frequency test unit

--- uuid_test.go
+++ uuid_test.go
@@ -17,7 +17,7 @@ func TestCollisions(t *testing.T) {
 		}
 		ids[id] = true
 		gen++
-		//println(i, " ┃ ", id, " ┃ ", col, " ┃ ", float64(col) / float64(gen))
+		println(i, " ┃ ", id, " ┃ ", col, " ┃ ", float64(col) / float64(gen))
 	}
 
 	println("Collected ", gen - col, " unique IDs out of ", gen, " generation call")
@@ -25,5 +25,25 @@ func TestCollisions(t *testing.T) {
 
 	if col != 0 {
 		t.Errorf("%d collisions found in %d generations", col, gen)
+	}
+}
+
+func TestFrequency(t *testing.T) {
+	chars := map[rune]int{}
+	gen := 0
+	id := NextUUID()
+	println("Generation start...")
+	for i := 0; i < 1 << 20; i++ {
+		id = NextUUID()
+		for _, b := range id {
+			if b != '-' {
+				chars[b]++
+				gen++
+			}
+		}
+	}
+	println("Generation end.")
+	for k, v := range chars {
+		println(string(k), " found ", v, " times out of ", gen, " entries. Frequency = ", float64(v) / float64(gen))
 	}
 }
